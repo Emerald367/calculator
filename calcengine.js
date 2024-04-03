32ea76fec8808ac761b4b9a102eb471e0272e753
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-04-03 11:07:16 -0700

    Made sure that all input is parsed

--- calcengine.js
+++ calcengine.js
@@ -1,21 +1,24 @@
+const math = require('mathjs');
+
 function calculate(a, b, operation) {
       switch(operation) {
           case 'add':
-            return a + b;
+            return math.evaluate(a + ' + ' + b);
             break;
 
           case 'subtract':
-            return a - b;
+            return math.evaluate (a + ' - ' + b);
             break;
 
           case 'multiply':
-            return a * b;
+            return math.evaluate(a + ' * ' + b);
             break;
 
           case 'division':
-            return a / b;
+            return math.evaluate(a + ' / ' + b);
             break;
       }
 }
 
 module.exports = calculate;
+
