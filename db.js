2ba5c8fc5fee3f6e0b5ed36e7ac107064a2b5cb5
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-05-07 11:05:25 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,6 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: 'localhost',
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: 5432,
+    connectionString: process.env.CONNECTION_STRING,
     ssl: {
         rejectUnauthorized: false
      }
